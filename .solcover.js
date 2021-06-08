9b270a4f2910fda1051cbc3fde782a94ae476ebd
Skylarmicz <skylarmic25@gmail.com>
2021-06-08 23:44:06 +0800

    Update .solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,2 @@
-module.exports = {
-    compileCommand: '../node_modules/.bin/truffle compile',
-    testCommand: '../node_modules/.bin/truffle test --network coverage',
-};
+export const compileCommand = '../node_modules/.bin/truffle compile';
+export const testCommand = '../node_modules/.bin/truffle test --network coverage';
